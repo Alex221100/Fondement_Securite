f545c2da18dd3b1a3df3201d7d090943793ae2d9
Alex221100 <71218218+Alex221100@users.noreply.github.com>
2022-01-03 14:56:51 +0100

    refactor(back) : add back project

---
+++ Back/Services/CertificateValidationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Back.Services
+{
+    public class CertificateValidationService
+    {
+        public bool ValidateCertificate(X509Certificate2 clientCertificate)
+        {
+            string[] allowedThumbprints = { "21A6E47A962C0E80A59517691FFA1D0500E3E548", "87A6E47A962C0E80A59517691FFA1D0500E3E548", "78A6E47A962C0E80A59517691FFA1D0500E3E548" };
+            if (allowedThumbprints.Contains(clientCertificate.Thumbprint))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
